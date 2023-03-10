2ee5f971193debc1a5a6b6534fc02bda619d4d9f
jSY49 <96411866+jSY49@users.noreply.github.com>
2023-03-10 16:05:25 +0900

    [add] Memo_Room&viewModel -> memo 추가 및 삭제

--- app/src/main/java/com/example/stickymemory/memoItemList/MemoItem.kt
+++ app/src/main/java/com/example/stickymemory/memoItemList/MemoItem.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.stickymemory.dataclasses.Memo
 
+
 @Composable
 fun MemoItem(item: Memo, onDelete: () -> Unit) {
 
