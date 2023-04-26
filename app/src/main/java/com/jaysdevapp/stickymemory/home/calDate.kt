42b9c09c1a1bf3c720eebe654803dcadb41036bd
jSY49 <jseung49@gmail.com>
2023-04-26 18:06:45 +0900

    [modify] 넘어온 시간 null 오류

--- app/src/main/java/com/jaysdevapp/stickymemory/home/calDate.kt
+++ app/src/main/java/com/jaysdevapp/stickymemory/home/calDate.kt
@@ -6,32 +6,37 @@ import java.util.*
 
 fun calDate(date: String?): String {
 
-    var dday_str = date!!.split("-").toMutableList()
-
-    val dateFormat = SimpleDateFormat("yyyyMMdd")
-    for (i in 1..2) {
-        if (dday_str[i].toInt() < 10) {
-            dday_str[i] = "0" + dday_str[i]
+    if (!date.isNullOrBlank()) {
+        var dday_str = date!!.split("-").toMutableList()
+        Log.d("cal", "$dday_str")
+        val dateFormat = SimpleDateFormat("yyyyMMdd")
+        for (i in 1..2) {
+            if (dday_str[i].toInt() < 10) {
+                dday_str[i] = "0" + dday_str[i]
+            }
+            dday_str[i] = dday_str[i].replace(" ", "")
         }
-        dday_str[i]=dday_str[i].replace(" ","")
-    }
-    Log.d("cal date", dday_str.toString())
-    val endDate = dateFormat.parse(dday_str[0] + dday_str[1] + dday_str[2]).time
+        Log.d("cal date", dday_str.toString())
+        val endDate = dateFormat.parse(dday_str[0] + dday_str[1] + dday_str[2]).time
 
-    val today = Calendar.getInstance().apply {
-        set(Calendar.HOUR_OF_DAY, 0)
-        set(Calendar.MINUTE, 0)
-        set(Calendar.SECOND, 0)
-        set(Calendar.MILLISECOND, 0)
-    }.time.time
-    val res = ((endDate - today) / (24 * 60 * 60 * 1000))
-    return if (res==0L) {
-        "D-Day"
-    } else if (res > 0) {
-        "D-" + res.toString()
-    } else {
-        "D+" + (res * (-1)).toString()
-    }
+        val today = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }.time.time
+        val res = ((endDate - today) / (24 * 60 * 60 * 1000))
+        return if (res == 0L) {
+            "D-Day"
+        } else if (res > 0) {
+            "D-" + res.toString()
+        } else {
+            "D+" + (res * (-1)).toString()
+        }
 
+    }
+    else{
+        return "none"
+    }
 }
 
